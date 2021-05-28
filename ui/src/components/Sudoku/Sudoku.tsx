89efe21dbd29194fa8ea722a3376801a2f9f2b72
Nhan Nguyen <ngthnhan93@gmail.com>
2021-05-28 00:28:06 -0700

    fix sudoku

--- ui/src/components/Sudoku/Sudoku.tsx
+++ ui/src/components/Sudoku/Sudoku.tsx
@@ -8,36 +8,54 @@ import {
 } from "__generated__/types";
 import React, { useEffect, useState } from "react";
 import {
+  Box,
+  createStyles,
+  Divider,
   Grid,
   LinearProgress,
   makeStyles,
+  Theme,
   Typography,
 } from "@material-ui/core";
 import { Fab } from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
 import NoteIcon from "@material-ui/icons/Note";
 import ClearIcon from "@material-ui/icons/Clear";
+import FormatAlignLeftIcon from "@material-ui/icons/FormatAlignLeft";
+import FormatAlignCenterIcon from "@material-ui/icons/FormatAlignCenter";
+import FormatAlignRightIcon from "@material-ui/icons/FormatAlignRight";
+import FormatBoldIcon from "@material-ui/icons/FormatBold";
+import FormatItalicIcon from "@material-ui/icons/FormatItalic";
+import FormatUnderlinedIcon from "@material-ui/icons/FormatUnderlined";
+import { PlayerList } from "components/Game";
 
-const useStyles = makeStyles({
-  layout: {
-    height: "100%",
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      border: `1px solid ${theme.palette.divider}`,
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: theme.palette.background.paper,
+      color: theme.palette.text.secondary,
+      "& svg": {
+        margin: theme.spacing(1.5),
+      },
+      "& hr": {
+        margin: theme.spacing(0, 0.5),
+      },
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    layout: {
+      height: "700px",
+    },
+  })
+);
 
 const Layout: React.FC = ({ children }) => {
   const classes = useStyles();
 
-  return (
-    <Grid
-      className={classes.layout}
-      container
-      direction="row"
-      justify="center"
-      alignItems="flex-start"
-    >
-      {children}
-    </Grid>
-  );
+  return <Box className={classes.layout}>{children}</Box>;
 };
 
 type SudokuProps = {
@@ -45,6 +63,7 @@ type SudokuProps = {
 };
 
 export const Sudoku: React.FC<SudokuProps> = ({ branchId }: SudokuProps) => {
+  const classes = useStyles();
   const [isFillInputMode, setIsFillInputMode] = useState(true); // If false -> Note input mode
   const { data, error, loading, subscribeToMore } = useGetFullBranchQuery({
     variables: {
@@ -105,34 +124,36 @@ export const Sudoku: React.FC<SudokuProps> = ({ branchId }: SudokuProps) => {
   return (
     <Layout>
       <BranchContextProvider id={branchId}>
-        <Grid item md={8}>
-          <Grid
-            container
-            direction="row"
-            justify="flex-start"
-            alignItems="flex-start"
-          >
-            <Grid item md={9}>
-              <Typography variant="h5">{branchId}</Typography>
+        <Grid container>
+          <Grid item md={8}>
+            <Grid
+              container
+              direction="row"
+              justify="flex-start"
+              alignItems="flex-start"
+            >
+              <Grid item md={9}>
+                <Typography variant="h5">{branchId}</Typography>
+              </Grid>
             </Grid>
+            <SudokuBoard
+              board={board}
+              inputMode={isFillInputMode ? "fill" : "note"}
+              toggleInputMode={toggleInputMode}
+            />
           </Grid>
-          <SudokuBoard
-            board={board}
-            inputMode={isFillInputMode ? "fill" : "note"}
-            toggleInputMode={toggleInputMode}
-          />
-        </Grid>
-        <Grid item md={4}>
-          <Fab
-            color={isFillInputMode ? "primary" : "secondary"}
-            onClick={toggleInputMode}
-          >
-            {isFillInputMode ? <EditIcon /> : <NoteIcon />}
-          </Fab>
-          <Fab color="secondary" onClick={toggleInputMode}>
-            <ClearIcon />
-          </Fab>
-          <History commits={commits} />
+          <Grid item md={4}>
+            <Fab
+              color={isFillInputMode ? "primary" : "secondary"}
+              onClick={toggleInputMode}
+            >
+              {isFillInputMode ? <EditIcon /> : <NoteIcon />}
+            </Fab>
+            <Fab color="secondary" onClick={toggleInputMode}>
+              <ClearIcon />
+            </Fab>
+            <History commits={commits} />
+          </Grid>
         </Grid>
       </BranchContextProvider>
     </Layout>
